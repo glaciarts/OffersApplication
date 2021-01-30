90fbbbe371853dbb672f24e8bc292fd184fbeead
Simon <szymon.softwarehouse@gmail.com>
2021-01-30 23:03:24 +0100

    Create OffersApplication.java

---
+++ OffersApplication.java
@@ -0,0 +1,25 @@
+package com.itsimple.offers;
+
+import com.itsimple.offers.service.files.FileStorageService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class OffersApplicationTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private FileStorageService storageService;
+
+    @Test
+    void contextLoads() {
+    }
+
+}
